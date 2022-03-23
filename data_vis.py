41d4e24cf6e4f5c0144cd571073ac163525b9bfd
Gran-Duma <brpenny@ualr.edu>
2022-03-23 14:57:35 -0500

    multiple subplots added to figure

--- data_vis.py
+++ data_vis.py
@@ -10,48 +10,59 @@ import time
 # import instrument ?
 class Animate:
 
-    def __init__(self,col_1_name,col_1_units):
+    def __init__(self,col_1_units,col_2_units):
 
-        self.fig,self.ax1 = plt.subplots()
+        self.fig,(self.ax1,self.ax2) = plt.subplots(2)
 
         self.col_0_name = 'Time'
         self.col_0_name = f'{self.col_0_name}'
 
-        self.col_1_name = col_1_name
+        self.col_1_name = 'Temperature'
         self.col_1_units = col_1_units
 
-        self.df = pd.DataFrame(columns=[self.col_0_name,self.col_1_name])
+        self.col_2_name = 'Resistance'
+        self.col_2_units = col_2_units
+    
+        self.df = pd.DataFrame(columns=[self.col_0_name,self.col_1_name,self.col_2_name])
         
     def Plot(self,i):
   
         # get time or query intstrument
         col_0 = (dt.datetime.now().strftime('%H:%M:%S.%f'))
         col_1 = eval(f'{self.col_1_name}_Reading()')
+        col_2 = eval(f'{self.col_2_name}_Reading()')
 
         # append end of dataframe
-  
-        self.df.loc[len(self.df.index)] = [col_0,col_1]
-
+        self.df.loc[len(self.df.index)] = [col_0,col_1,col_2]
 
         # only show last entries of dataframe to avoid clutter
         self.df1 = self.df.tail(7)
 
         # clear plot to avoid plotting same stuff on top of eachother
-        plt.cla()
+        self.ax1.clear()
 
         self.ax1.set_title(f'{self.col_1_name} vs. {self.col_0_name}')
         self.ax1.set_xlabel(self.col_0_name)
         self.ax1.set_ylabel(f'{self.col_1_name} ({self.col_1_units})')
 
-
         self.df1.plot(x=self.col_0_name,y=self.col_1_name,ax=self.ax1)
-        
+
         # needed due to appending nature of data
         for label in self.ax1.get_xticklabels():
             label.set_ha("right")
             label.set_rotation(45)
 
         self.ax1.set_ylim([0, 273])
+
+        self.ax2.clear()
+
+        self.ax2.set_title(f'{self.col_2_name} vs. {self.col_1_name}')
+        self.ax2.set_xlabel(self.col_1_name)
+        self.ax2.set_ylabel(f'{self.col_2_name} ({self.col_2_units})')
+
+        self.df1.plot(x=self.col_1_name,y=self.col_2_name,ax=self.ax2)
+
+        plt.tight_layout()
 
     def Animation(self):
         self.ani = animation.FuncAnimation(self.fig,self.Plot,interval=1000)
@@ -81,7 +92,7 @@ class Animate:
 def Temperature_Reading():
 
     temp = random.randint(5,230)
-
+    
     # temp = instrument.GetTemp() ?
 
     return temp
@@ -104,19 +115,15 @@ def ani_close():
 
     plt.close('all')
 
-def Temp_V_Time():
-    global Temp_Graph
-    Temp_Graph = Animate('Temperature','K')
-    Temp_Graph.Animation()
+def Visuals():
+    global Graph
+    Graph = Animate('K','mOhm')
+    Graph.Animation()
 
-def Temp_V_Time_Save():
-    Temp_Graph.save_data()
-
-def Res_V_Time():
-    global Res_Graph
-    Res_Graph = Animate('Resistance','mOhm')
-    Res_Graph.Animation()
+def Data_Save():
+    Graph.save_data()
 
 if __name__ == '__main__':
-    Temp_V_Time()
+
+    Visuals()
  
--- gui_control.py
+++ gui_control.py
@@ -21,7 +21,7 @@ def run_gui():
     button_1 = tk.Button(root, text ='Load Instructions', command=instruct)
     button_1.grid(row=0,column=0,sticky=tk.EW)
 
-    button_2 = tk.Button(root, text ='Start Visuals', command=dv.Temp_V_Time)
+    button_2 = tk.Button(root, text ='Start Visuals', command=dv.Visuals)
     button_2.grid(row=1,column=0,sticky=tk.EW)
 
     button_3 = tk.Button(root, text ='Save Data', command=save)
@@ -36,7 +36,7 @@ def run_gui():
 def save():
     """assign thread to call data saving function"""
 
-    t1 = threading.Thread(target=dv.Temp_V_Time_Save)
+    t1 = threading.Thread(target=dv.Data_Save)
     t1.start()
 
 def instruct():
